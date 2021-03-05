8f4716b5f82437f036d02e0c9b42ae65a1579239
diana <dianantb@kth.se>
2021-03-05 19:52:11 +0100

    re-structuturing and added more methods

--- app/src/main/java/todolistmaster/Main.java
+++ app/src/main/java/todolistmaster/Main.java
@@ -4,87 +4,16 @@ import java.util.Scanner;
 
 public class Main {
 
-   /* Integer show = 1;
-    Integer add = 2;
-    Integer edit = 3;
-    Integer saveQuit = 4;*/
-
-    String userOption;
-
-
-
-    public static void printWelcome() {
-        System.out.println();
-        System.out.println("Welcome to ToDoLy");
-        System.out.println("You have " + "X - placeholder" + "tasks todo and " + "Y - placeholder" + "tasks are done!");
-        System.out.println("Pick an option:");
-        System.out.println("(" + 1 + ")" + "Show Task List (by date or project)");
-        System.out.println("(" + 2 + ")"+ "Add New Task");
-        System.out.println("(" + 3 + ")" + "Edit Task (update, mark as done, remove)");
-        System.out.println("(" + 4 + ")" + "Save and Quit");
-
-    }
-    public void setUserCommand(String userOption) {
-        Scanner userCommand = new Scanner(System.in);
-        getUserCommand(userCommand.nextLine());
-
-    }
-
-    public String getUserCommand(String userOption) {
-        // if loop validating user command as well as connecting to the appropriate course of action
-        // if command invalid then display error and call method printWelcome again }
-        return this.userOption = userOption;
-
-    }
-
-
-    public processUserCommand(){
-        switch (userOption == "1"){
-            // call method Show Task list
-            // break
-
-            // call method add new Task
-            // break
-
-            // call method edit task
-            // break
-
-            // call save and quit
-            // break
-        }
-
-
-    }
-
-
-
-    // Create a task
-    public void addTaskToArrayList() {
-        Task t = new Task;
-        t.createTask();
-
-        ArrayList<Task> listOfTasks = new ArrayList<>();
-        listOfTasks.add(t);
-
-    }
-
-    public void removeTaskfromArrayList(){
-        // display array
-        // select task by writing the id ?
-        // remove id which removes the entire task
-
-    }
-
-
     public static void main(String[] args) {
 
         // display initial instruction of TodoLy
-        printWelcome();
+        Todolist.printWelcome();
 
 
 
 
     }
+
 
 }
 
--- app/src/main/java/todolistmaster/Todolist.java
+++ app/src/main/java/todolistmaster/Todolist.java
@@ -1,5 +1,9 @@
 package todolistmaster;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
 Saves taks into an Arraylist (adds)
 removes task from an Arraylist
@@ -10,7 +14,77 @@ validate commands against pre-defined list and calls the other methods above
 
 public class Todolist {
 
-    public static
+
+    Integer userOption;
+    ArrayList<Task> listOfTasks = new ArrayList<>();
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void printWelcome() {
+        System.out.println();
+        System.out.println("Welcome to ToDoLy");
+        System.out.println("You have " + "X - placeholder" + "tasks todo and " + "Y - placeholder" + "tasks are done!");
+        System.out.println("Pick an option:");
+        System.out.println("(" + 1 + ")" + "Show Task List (by date or project)");
+        System.out.println("(" + 2 + ")" + "Add New Task");
+        System.out.println("(" + 3 + ")" + "Edit Task (update, mark as done, remove)");
+        System.out.println("(" + 4 + ")" + "Save and Quit");
+
+    }
+
+    public void setUserCommand(Integer userOption) {
+        userOption = scanner.nextInt();
+
+    }
+
+    public int getUserCommand(Integer userOption) {
+        // if loop validating user command as well as connecting to the appropriate course of action
+        // if command invalid then display error and call method printWelcome again }
+        return this.userOption = userOption;
+
+    }
+
+
+    public void processUserCommand() {
+     //   int[] displayedOptions = {1, 2, 3, 4};
+        if (userOption == 1) {
+            showTaskList();
+        } else if (userOption == 2) {
+            addTaskToArrayList();
+        } else if (userOption == 3) {
+            // call method edit task
+        } else if (userOption == 4) {
+            // call save and quit
+        } else {
+            // return error and display options again
+        }
+    }
+
+    public void showTaskList() {
+        System.out.println(listOfTasks);
+
+    }
+
+
+    // Create a task
+    public void addTaskToArrayList() {
+        Task t = new Task;
+        t.createTask();
+
+        listOfTasks.add(t);
+
+    }
+
+    public void removeTaskfromArrayList() {
+        // display array
+        // select task by writing the id ?
+        // remove id which removes the entire task
+
+    }
+
+}
+
+
 
 
 
