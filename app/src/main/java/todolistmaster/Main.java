53e042a403332eceea3ed3806b8205b6296ddcda
Diana Bao <dianantb@gmail.com>
2021-03-13 21:07:40 +0100

    final of the day

--- app/src/main/java/todolistmaster/Main.java
+++ app/src/main/java/todolistmaster/Main.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 /** This is where the program runs. This class calls the objects, instantiate and use/call methods for the todolist app execution in appropriate order
  **/
 
+// TODO btw: what is the app package here? is that just a test one?
+
 public class Main {
 
     public static void main(String[] args) {
